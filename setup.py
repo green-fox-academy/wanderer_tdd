1772cb27c84db132fb03f14fe1d1a9356cd12e81
gy0p4k <gyorgypakozdi@gmail.com>
2018-06-05 10:40:28 +0200

    chore(): some structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wanderer_tdd',
+    packages=find_packages(),
+    version='0.0.1',
+    description='',
+    long_description='',
+    author='GFA',
+    author_email=''
+)
