99b6e8c0192fa0f59c5d687c5ac73f8c82861225
gy0p4k <gyorgypakozdi@gmail.com>
2018-06-05 10:52:01 +0200

    first test succes, refactor

---
+++ models/hero.py
@@ -0,0 +1,6 @@
+from config import hero as hero_config
+
+class Hero:
+    
+    def __init__(self):
+        self.hp = hero_config.BASE_HP
--- tests/test_hero.py
+++ tests/test_hero.py
@@ -1,5 +1,6 @@
 from models.hero import Hero
+from config import hero as hero_config
 
 def test_hero_initialize():
     test_hero = Hero()
-    assert test_hero.hp == 10
+    assert test_hero.hp == hero_config.BASE_HP
